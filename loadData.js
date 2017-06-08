388c4514c982e727698e372837231b6cc0ddce2d
Yannick Assogba <yassogba@gmail.com>
2017-06-08 15:14:59 -0400

    add support for csv and tsv files. Fix bug in detecting json files.

--- loadData.js
+++ loadData.js
@@ -1,27 +1,44 @@
 const path = require('path');
 const fs = require('fs');
+const d3Dsv = require('d3-dsv');
+const chardet = require('jschardet');
+
+
+function loadString(fp) {
+  const fileBuffer = fs.readFileSync(fp);
+  const encoding = chardet.detect(fileBuffer);
+  const data = fileBuffer.toString(encoding.encoding);
+  return data;
+}
 
 function loadJSON(fp) {
-  const fileData = fs.readFileSync(fp);
-  return JSON.parse(fileData);
+  const data = loadString(fp);
+  return JSON.parse(data);
 }
 
 function loadTSV(fp) {
-  return undefined;
+  const data = loadString(fp);
+  return d3Dsv.tsvParse(data);
+}
+
+function loadCSV(fp) {
+  const data = loadString(fp);
+  return d3Dsv.csvParse(data);
 }
 
 function load(fp) {
   const ext = path.extname(fp);
-  if (ext === 'json') {
+  console.log("EXT", ext)
+  if (ext === '.json') {
     return loadJSON(fp);
-  } else if (ext === 'tsv') {
-    return loadTSV();
+  } else if (ext === '.tsv') {
+    return loadTSV(fp);
+  } else if (ext === '.csv') {
+    return loadCSV(fp);
   }
   return undefined;
 }
 
 module.exports = {
   load,
-  loadJSON,
-  loadTSV,
 };
