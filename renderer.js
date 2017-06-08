affce39e8bbfad62fe5af737b1a998d7ab31e676
Yannick Assogba <yassogba@gmail.com>
2017-06-08 14:29:04 -0400

    cleanup lint stuff

--- renderer.js
+++ renderer.js
@@ -1,23 +1,23 @@
 const libVoyager = require('voyager');
-const { ipcRenderer } = require('electron')
+const { ipcRenderer } = require('electron');
 
 const config = {
   showDataSourceSelector: false,
-}
+};
 let data;
 
-const voyagerInstance = libVoyager.CreateVoyager("#voyager-embed", config, data)
+const voyagerInstance = libVoyager.CreateVoyager('#voyager-embed', config, data);
 
 setTimeout(() => {
   // Tell main thread we are ready.
-  ipcRenderer.send('status', 'ready')
+  ipcRenderer.send('status', 'ready');
 }, 50);
 
 
 ipcRenderer.on('data', (event, arg) => {
   data = arg;
   voyagerInstance.updateData(data);
-})
+});
 
 
 
