20b5ef8bd8d51635e9c572d7d316acfdc5718c60
Yannick Assogba <yassogba@gmail.com>
2017-06-09 16:24:15 -0400

    WIP drag and drop spike

--- renderer.js
+++ renderer.js
@@ -6,21 +6,39 @@ const config = {
 };
 let data;
 
-const voyagerInstance = libVoyager.CreateVoyager('#voyager-embed', config, data);
+const container = document.getElementById('voyager-embed');
+const voyagerInstance = libVoyager.CreateVoyager(container, config, data);
+
+
+// Set up drag and drop
+const dragTarget = document.getElementById('title-area');
+dragTarget.ondragover = function handleOnDragOver() {
+  return false;
+};
+
+dragTarget.ondragleave = function handleDragLeave() {
+  return false;
+};
+
+dragTarget.ondrop = function handleOnDrop(e) {
+  e.preventDefault();
+  console.log('event', e);
+  const file = e.dataTransfer.files[0];
+  console.log('file', file);
+  return false;
+};
+
+
 
 setTimeout(() => {
   // Tell main thread we are ready.
   ipcRenderer.send('status', 'ready');
 }, 50);
 
-
+// Channel to receive data from main thread.
 ipcRenderer.on('data', (event, arg) => {
   data = arg;
-  console.log('data received', data)
   voyagerInstance.updateData({
     values: data,
   });
 });
-
-
-
