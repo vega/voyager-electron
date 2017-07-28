29913d297a4472064bcdccc385bd66d1b8fde627
Felix Ouk <felix.ouk@gmail.com>
2017-07-28 15:27:22 -0700

    Rename 'open' in menu bar to 'load csv or json data'

--- menu.js
+++ menu.js
@@ -62,7 +62,7 @@ function createMenuTree(app, handlers) {
       role: 'window',
       submenu: [
         {
-          label: 'Open',
+          label: 'Load CSV or JSON data',
           click: handlers.handleOpen,
         },
         {
